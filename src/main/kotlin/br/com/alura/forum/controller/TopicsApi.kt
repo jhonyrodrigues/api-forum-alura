ab46d18df677d5832141008b1ce033f67c6e544e
Jhony de Oliveira Rodrigues <jhony.o_rodrigues@hotmail.com>
2022-02-11 19:24:13 -0300

    adicionando recurso de cache nos métodos no controller.

--- src/main/kotlin/br/com/alura/forum/controller/TopicsApi.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicsApi.kt
@@ -3,11 +3,12 @@ package br.com.alura.forum.controller
 import br.com.alura.forum.domain.dto.CreateTopicRequest
 import br.com.alura.forum.domain.dto.TopicResponse
 import br.com.alura.forum.domain.dto.UpdateTopicRequest
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.util.UriComponentsBuilder
 import javax.validation.Valid
 
 @RestController
@@ -15,6 +16,7 @@ import javax.validation.Valid
 interface TopicsApi {
 
     @GetMapping
+    @Cacheable("topics")
     fun list(): List<TopicResponse>
 
     @GetMapping("/{id}")
@@ -22,14 +24,17 @@ interface TopicsApi {
 
     @PostMapping
     @Transactional
-    fun create(@RequestBody @Valid createTopicRequest: CreateTopicRequest, uri: UriComponentsBuilder): ResponseEntity<TopicResponse>
+    @CacheEvict(value = ["topics"], allEntries = true)
+    fun create(@RequestBody @Valid createTopicRequest: CreateTopicRequest): ResponseEntity<TopicResponse>
 
     @PutMapping("{id}")
     @Transactional
+    @CacheEvict(value = ["topics"], allEntries = true)
     fun update(@PathVariable id: Long, @RequestBody @Valid updateTopicRequest: UpdateTopicRequest): ResponseEntity<TopicResponse>
 
     @DeleteMapping("{id}")
     @Transactional
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @CacheEvict(value = ["topics"], allEntries = true)
     fun delete(@PathVariable id: Long)
 }
